cbfff58ac5cd1b6681fb2f384bc55e17b1c7d749
A <Andreyfox.ru@gmail.com>
2019-09-27 21:39:35 +0300

    Bug with showing data is fixed

--- WpfApp1/CarsData.cs
+++ WpfApp1/CarsData.cs
@@ -9,8 +9,8 @@ namespace WpfApp1
 {
     class CarsData
     {
-        public ObservableCollection<Car> Cars;
-        public Car SelectedCar;
+        public ObservableCollection<Car> Cars { get; set; }
+        public Car SelectedCar { get; set; }
         public CarsData()
         {
             Cars = new ObservableCollection<Car>{ new Car("Toyota", "Yelow", "2005"), new Car("Audi", "Red", "2011"), new Car("Porshe", "White", "2000") };
